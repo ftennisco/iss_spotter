47b1acb777f41215b5a1158d1cbcc0ffe9b045e7
ftennisco <f.tennisco@hotmail.com>
2023-06-20 15:59:16 -0700

    ISS spotter code containing fetchMyIP, fetchCoordsByIP, and fetchISSFlyOverTimes.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const iss = require('./iss');
 
+iss.fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+
+  console.log('It worked! Returned IP:', ip);
+});
+
+iss.fetchCoordsByIP('70.67.13.77', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+const exampleCoords = { latitude: '48.4284207', longitude: '-123.3656444' };
+
+iss.fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
--- iss.js
+++ iss.js
@@ -1,1 +1,69 @@
+const request = require('request');
 
+// the code should pass through the error to the callback 
+// if an error occurs when requesting the IP data
+
+// parse and extract the IP address using JSON and then pass that 
+// through to the callback (as the second argument) if there is no error
+
+const fetchMyIP = function(callback) {
+  const url = 'https://api.ipify.org?format=json';
+
+  request(url, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const  parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+    }
+
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, {latitude, longitude});
+  });
+};;
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetchiong ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+module.exports = { 
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+};
